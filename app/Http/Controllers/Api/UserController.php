996324d06b95179f78982553fdf8e74e4d872335
Kirill Arutyunov <kirill@arutyunov.me>
2018-11-25 07:41:27 +0200

    Fix account type

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -28,7 +28,7 @@ class UserController extends Controller
      */
     public function signup(UserSignupRequest $request, UserRepository $repository, ApiAuth $apiAuth)
     {
-        $user = $repository->create($request->username, $request->password, $request->type);
+        $user = $repository->create($request->username, $request->password, (int)$request->type);
         $token = $apiAuth->make($user);
 
         //TODO make resource
